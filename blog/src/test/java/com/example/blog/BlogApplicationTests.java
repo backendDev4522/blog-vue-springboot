5bfd998914ec7624345144f24b2b3acc27adbee2
seongjun2 <dhzm2aud@naver.com>
2019-12-12 14:41:01 +0900

    junit member VO
    lombok & jpa

--- blog/src/test/java/com/example/blog/BlogApplicationTests.java
+++ blog/src/test/java/com/example/blog/BlogApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.blog;
 
+import com.example.blog.api.domain.Member;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,13 @@ class BlogApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("Hello world!!");
 	}
 
+	@Test
+	void memberLombokTest(){
+		Member member = Member.builder().id("id").password("pwd").build();
+		member.setEmail("dhzm2aud@naver.com");
+		System.out.println("lombok test : " + member);
+	}
 }
