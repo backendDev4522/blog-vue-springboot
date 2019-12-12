2391de08490a9a3bfac71b25dd3714a1688059bd
김건우 <kgw1457@gmail.com>
2019-12-12 18:03:01 +0900

    add junit test

--- blog/src/test/java/com/example/blog/BlogApplicationTests.java
+++ blog/src/test/java/com/example/blog/BlogApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.blog;
 
+import java.util.Calendar;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.blog.api.domain.Board;
 
 @SpringBootTest
 class BlogApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void lombokTest() {
+		Board board = Board.builder()
+				.bid(1)
+				.title("exam")
+				.content("examlple")
+				.boardType("자유게시판")
+				.createdDate(Calendar.getInstance().getTime())
+				.build();
 	}
-
+	
 }
