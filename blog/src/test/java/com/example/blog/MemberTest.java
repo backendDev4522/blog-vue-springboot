6f3831cbf2fccf1642fa3254f9a08a90e787f161
=김건우 <=kin0984@naver.com>
2019-12-13 17:51:45 +0900

    Add MemberTest

---
+++ blog/src/test/java/com/example/blog/MemberTest.java
@@ -0,0 +1,13 @@
+package com.example.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MemberTest {
+
+	@Test
+	public void test() {
+		System.out.println("Test");
+	}
+}
