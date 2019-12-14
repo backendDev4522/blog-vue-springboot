06916b44ef30a6831d56f311c9593e7149fb232c
=김건우 <=kin0984@naver.com>
2019-12-14 17:36:25 +0900

    Modify entities for jpa audit

--- blog/src/main/java/com/example/blog/api/domain/Board.java
+++ blog/src/main/java/com/example/blog/api/domain/Board.java
@@ -1,17 +1,11 @@
 package com.example.blog.api.domain;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
-import org.springframework.data.annotation.CreatedDate;
 
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -23,36 +17,34 @@ import lombok.ToString;
 @Entity
 @Getter
 @ToString
-@Table(name="board")
-public class Board {
+@Table(name = "board")
+public class Board extends TimeEntity{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long bid;
-	
-	/* 연관관계 설정부분.
+
+	/*
+	 * 연관관계 설정부분.
+	 * 
 	 * @ManyToOne
+	 * 
 	 * @JoinColumn(name="member_id") private Member member;
 	 */
-	
-	//먼저 연관관계를 생각하지 않은 CRUD 게시판을 만든 후 연관관계를 설정해준다.
+	// 먼저 연관관계를 생각하지 않은 CRUD 게시판을 만든 후 연관관계를 설정해준다.
 	@Column(nullable = false, length = 20)
 	private String writer;
-	
+
 	@Column(nullable = false, length = 50)
 	private String title;
-	
+
 	@Column(nullable = false, columnDefinition = "TEXT")
 	private String content;
-	
+
 	@Column(nullable = false, length = 50)
 	private String boardType;
-	
-	@CreatedDate
-    @Column(updatable = false, name="reg_date")
-	private Date regDate;
-	
-	
+
+
 	@Builder
 	private Board(String writer, String title, String content, String boardType) {
 		this.writer = writer;
@@ -60,8 +52,5 @@ public class Board {
 		this.content = content;
 		this.boardType = boardType;
 	}
-	
-	
-	
-	
+
 }
---
+++ blog/src/main/java/com/example/blog/api/domain/TimeEntity.java
@@ -0,0 +1,28 @@
+package com.example.blog.api.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class TimeEntity {
+	
+	@Column(name="created_date")
+	@CreatedDate
+	private LocalDateTime createdDate;
+	
+	@Column(name="update_date")
+	@LastModifiedDate
+	private LocalDateTime updatedDate;
+	
+}
