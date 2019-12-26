241b45284b79f507a5587ec87d97e4892faffa11
seongjun2 <dhzm2aud@naver.com>
2019-12-26 16:50:51 +0900

    Member feature update
    controller v1.0

--- blog/src/main/java/com/example/blog/api/controller/MemberController.java
+++ blog/src/main/java/com/example/blog/api/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,8 +31,8 @@ class MemberRestController{
     }
 
     @PostMapping("save")
-    public ResponseEntity save(@RequestBody final MemberDto memberDto){
-        memberService.save(memberDto);
+    public ResponseEntity save(@RequestBody final MemberDto dto){
+        memberService.save(dto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
@@ -47,4 +48,11 @@ class MemberRestController{
         MemberDto dto = memberService.searchById(id);
         return new ResponseEntity<>(dto,HttpStatus.OK);
     }
+
+    @PutMapping("update")
+    public ResponseEntity update(@RequestBody final MemberDto dto){
+        memberService.update(dto);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
